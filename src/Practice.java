be7056cb73f74a928575e320b46ef98765eeb6ea
devasishp305 <debasishp898@gmail.com>
2021-11-05 00:02:06 +0530

    3rd commit

--- src/Practice.java
+++ src/Practice.java
@@ -2,5 +2,7 @@ public class Practice {
     public static void main(String[] args){
         System.out.println("practice");
         System.out.println("practice no");
+        System.out.println("practice yes");
+
     }
 }
